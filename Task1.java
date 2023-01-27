77ed1f7f661b8c925841949f2dd835368d670690
Irina_Ryzhenko <platinumfoxirina@gmail.com>
2023-01-27 12:45:17 +0300

    solutions for task 1-3

---
+++ Task1.java
@@ -0,0 +1,15 @@
+package java_hometasks;
+
+public class Task1 {
+    public static void main (String[] args) {
+        int [] array = {11, 10, 15, 5}; // числом "n" по условиям задачи будем 10
+        int sum = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != 10) {
+                sum = sum + array[i];
+            }
+        }
+        System.out.println(sum);
+    }
+}
