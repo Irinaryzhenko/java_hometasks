64df7e444bb9c0360f70aa3ec2ca2f554fb72a1b
Irina_Ryzhenko <platinumfoxirina@gmail.com>
2023-02-08 10:53:57 +0300

    changes

---
+++ homework2/Task6.java
@@ -0,0 +1,15 @@
+package java_hometasks.homework2;
+
+public class Task6 {
+    public static void main( String[] args ) {
+        String text = "Imagination rules the world";
+        String[] words = text.split( " " );
+        String element = " ";
+
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].length() > element.length())
+                element = words[i];
+        }
+        System.out.println(element);
+    }
+}
---
+++ homework2/Task9.java
@@ -0,0 +1,18 @@
+package java_hometasks.homework2;
+
+public class Task9 {
+    public static void main (String[] args) {
+        String greatText = "Never stop dreaming";
+        String[] arrays = greatText.split(" ");
+
+        String neededWord = "stop";
+        for (String array : arrays) {
+            if (array.contains("stop")) {
+                int index = neededWord.indexOf('s');
+
+                System.out.println(index);
+
+            }
+        }
+    }
+}
